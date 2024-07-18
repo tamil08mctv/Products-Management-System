edbf617946488002bcc09eaa86bdb57dd8fa54ad
tamil08mctv <tamil08mctv@gmail.com>
2024-07-18 19:16:04 +0530

    Initiated delete and update operations

--- Products/views.py
+++ Products/views.py
@@ -2,21 +2,16 @@ from .models import Products
 from .serializers import ProductsSerializer
 from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
 
 class ProductsListCreate(generics.ListCreateAPIView):
     queryset = Products.objects.all()
     serializer_class = ProductsSerializer
-
 
 class ProductsRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Products.objects.all()
     serializer_class = ProductsSerializer
     lookup_field = "id"
 
-
-class ProductDelete(APIView):
     def delete(self, request, *args, **kwargs):
         product_id = kwargs.get('id')
         try:
