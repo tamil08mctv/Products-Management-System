2555183ab56cb6bd8b2ea30e215156667ff78e8c
tamil08mctv <tamil08mctv@gmail.com>
2024-07-18 15:31:13 +0530

    PMS delete function done

--- Products/views.py
+++ Products/views.py
@@ -1,18 +1,27 @@
 from .models import Products
 from .serializers import ProductsSerializer
-from rest_framework import generics,status
+from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 class ProductsListCreate(generics.ListCreateAPIView):
     queryset = Products.objects.all()
-    serializer_class= ProductsSerializer
+    serializer_class = ProductsSerializer
 
-    def delete(self,request,*args,**kwargs):
-        Products.objects.all.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)                                                            
 
 class ProductsRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset=Products.objects.all()
-    serializer_class= ProductsSerializer
-    lookup_field="id"
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+    lookup_field = "id"
+
+
+class ProductDelete(APIView):
+    def delete(self, request, *args, **kwargs):
+        product_id = kwargs.get('id')
+        try:
+            product = Products.objects.get(id=product_id)
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Products.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
